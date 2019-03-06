5b6e2fb54a25736adce3999db16f1ab816338565
Najib Adan <najibadan@airmail.cc>
2019-03-06 11:26:51 +0300

    Recursively search the b-tree

--- db.c
+++ db.c
@@ -307,34 +307,31 @@ void initialize_internal_node(void* node) {
 }
 
 
-Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
+Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key) {
   void* node = get_page(table->pager, page_num);
-  uint32_t num_cells = *leaf_node_num_cells(node);
+  uint32_t num_keys = *internal_node_num_keys(node);
 
-  Cursor* cursor = malloc(sizeof(Cursor));
-  cursor->table = table;
-  cursor->page_num = page_num;
-
-  // Binary search
+  /* Binary search to find index of child to search */
   uint32_t min_index = 0;
-  uint32_t one_past_max_index = num_cells;
-  while (one_past_max_index != min_index) {
-    uint32_t index = (min_index + one_past_max_index) / 2;
-    uint32_t key_at_index = *leaf_node_key(node, index);
-    if (key == key_at_index) {
-      cursor->cell_num = index;
-      return cursor;
-    }
-    if (key < key_at_index) {
-      one_past_max_index = index;
+  uint32_t max_index = num_keys; /* there is one more child than key */
+
+  while (min_index != max_index) {
+    uint32_t index = (min_index + max_index) / 2;
+    uint32_t key_to_right = *internal_node_key(node, index);
+    if (key_to_right >= key) {
+      max_index = index;
     } else {
       min_index = index + 1;
     }
   }
-
-  cursor->cell_num = min_index;
-  return cursor;
-
+  uint32_t child_num = *internal_node_child(node, min_index);
+  void* child = get_page(table->pager, child_num);
+  switch (get_node_type(child)) {
+    case NODE_LEAF:
+      return leaf_node_find(table, child_num, key);
+    case NODE_INTERNAL:
+      return internal_node_find(table, child_num, key);
+  }
 }
 
 //get_page() method has the logic for handling a cache miss
@@ -636,8 +633,7 @@ Starting from the right, move each key to correct position.
   if (is_node_root(old_node)) {
     return create_new_root(cursor->table, new_page_num);
   } else {
-    printf("Need to implement updating parent after split\n");
-    exit(EXIT_FAILURE);
+    return internal_node_find(table, root_page_num, key);
   }
 }
 // inserting a key/value pair into a leaf node.
